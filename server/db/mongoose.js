42ce32159ecb5eeb05e95a490a9ef966b63bbb83
kingjamesegun <kingjamesegun@gmail.com>
2020-05-02 04:48:55 +0100

    trying it

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb+srv://kingjames:jamesegun@cluster0-ohmw1.mongodb.net/test?retryWrites=true&w=majority" )
+mongoose.connect("mongodb+srv://kingjames:jamesegun@cluster0-ohmw1.mongodb.net/test?retryWrites=true&w=majority" || "mongodb://localhost:27017/TodoApp" )
   
 
 module.exports = {mongoose};
